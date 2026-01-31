444f96571da92b206b72e9dd92b37294e4e76bf7
DanielLeo000 <danieljunioleo2020@gmail.com>
2026-01-31 11:57:55 -0300

    Ajustes interface do administrador

--- public/JS/app.js
+++ public/JS/app.js
@@ -1,48 +1,39 @@
 const url = `http://localhost:3000/produtos`;
 
-const btn = document.querySelector('.menu-toggle');
-const nav = document.querySelector('.nav-links');
-const icon = document.getElementById('ToogleIcon');
 const listagem = document.getElementById('list');
+const tabela = document.getElementById('produtos-list');
 
 let produtos = [];
 
-btn.addEventListener('click', () => {
-    nav.classList.toggle('active');
-    icon.innerHTML = nav.classList.contains('active') ? '✖' : '☰';
-});
-
-const links = document.querySelectorAll('.nav-links li');
-
-links.forEach(link => {
-    link.addEventListener('click', () =>{
-         nav.classList.remove('active');
-    })
-})
-
-
-
-function ObterDados(){
-   fetch(url)
-   .then(res => {
-        if(!res.ok){
-            console.log("ERRO HTTP : " + res.status);
-            return;
-        }
-        return res.json();
-   })
-   .then(data => {
-        produtos = data;
-        console.log(produtos);
-        Listar(produtos);
-   })
-   .catch(erro => console.error("Erro ao buscar produtos:", erro));
+ function ObterDados() {
+    fetch(url)
+        .then(res => {
+            if (!res.ok) {
+                console.log("ERRO HTTP : " + res.status);
+                return;
+            }
+            return res.json();
+        })
+        .then(data => {
+            produtos = data;
+            if (listagem) {
+                Listar(produtos);
+            }
+            if (tabela) {
+                listarProdutosTabela();
+            }
+        })
 }
 
-function Listar(produtos){
-    let stx ="";
-    produtos.forEach(pro => {
-         stx += ` <div class="produto">
+window.addEventListener('load', () => {
+    ObterDados();
+});
+
+function Listar(array) {
+    if (!listagem) return; 
+    let stx = "";
+    array.forEach(pro => {
+        stx += ` <div class="produto">
                     <img src="${pro.imagem}" alt="Camisa 1">
                     <div class="informacoes">
                         <h3>${pro.titulo}</h3><span class="preco">${pro.preco}</span>
@@ -55,5 +46,21 @@ function Listar(produtos){
     listagem.innerHTML = stx;
 }
 
-window.addEventListener('load',ObterDados);
+
+async function listarProdutosTabela() {
+    const tabela = document.getElementById('produtos-list');
+    if (!tabela) return; 
+    let stx = "";
+    produtos.forEach(produto => {
+        stx += `<tr data-id="${produto.id}">
+                    <td data-label="Título">${produto.titulo}</td>
+                    <td data-label="Preço">R$ ${parseFloat(produto.preco).toFixed(2)}</td>
+                    <td data-label="Quantidade">${produto.quantidade}</td>
+                </tr>`;
+    })
+    tabela.innerHTML = stx;
+}
+
+
+
 
